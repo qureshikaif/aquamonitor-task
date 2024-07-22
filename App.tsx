c777514e322025b4385cf32ad4718d0eb862dd8d
qureshikaif <qureshik488@gmail.com>
2024-07-23 02:24:45 +0500

    feat: added react navigation dependencies

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Weight from './src/screens/Profile/Weight';
+import {NavigationContainer} from '@react-navigation/native';
 
 function App(): React.JSX.Element {
-  return <Weight />;
+  return (
+    <NavigationContainer>
+      <Weight />
+    </NavigationContainer>
+  );
 }
 
 export default App;
